22d0198f6bfcb53171ddcd2e379150fcda71e229
smrn001 <samir.niroula001@gmail.com>
2024-10-30 20:49:03 +0545

    some weierd commit

--- app/(docs-page)/components/page.tsx
+++ app/(docs-page)/components/page.tsx
@@ -7,29 +7,12 @@ import Image from 'next/image';
 import preview from '@/assets/preview';
 const appsDesign = [
   {
-    id: 'motionNumber',
-    name: 'motion number',
-    url: '/components/motion-number',
-    imgSrc: preview.motionNumber,
-  },
-  {
     id: 'buttons',
     url: '/components/buttons',
     name: 'buttons',
     imgSrc: preview.buttons,
   },
-  {
-    id: 'horizontal-scrolling',
-    url: '/components/horizontal-scroll',
-    name: 'scrolling',
-    imgSrc: preview.horizontascroll,
-  },
-  {
-    id: 'clip-path',
-    url: '/components/clip-path',
-    name: 'Clip-Path',
-    imgSrc: preview.clippath,
-  },
+
   {
     id: 'image-reveal',
     url: '/components/product-cards',
